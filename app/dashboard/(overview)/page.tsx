6ddfe05c3c10d5ca75bb2fd35bbd7f095a5b4775
Daniel Garcia <124107401+danvgar@users.noreply.github.com>
2024-01-12 00:42:27 -0800

    Added Suspense to LatestInvoices component, with fallback to LatestInvoicesSkeleton. Removed props from LatestInvoices component, fetching its own data now from @/app/lib/data

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,12 +2,12 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart'
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices'
 import { lusitana } from '@/app/ui/fonts'
-import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+import { fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { RevenueChartSkeleton, LatestInvoicesSkeleton } from '@/app/ui/skeletons';
 
 export default async function Page() {
-    const latestInvoices = await fetchLatestInvoices();
+
     const {
         numberOfInvoices,
         numberOfCustomers,
@@ -34,7 +34,11 @@ export default async function Page() {
                 <Suspense fallback={<RevenueChartSkeleton />}>
                     <RevenueChart />
                 </Suspense>
-                <LatestInvoices latestInvoices={latestInvoices} />
+                <Suspense fallback={<LatestInvoicesSkeleton />}>
+                    <LatestInvoices />
+                </Suspense>
+                {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+
             </div>
         </main>
     );
