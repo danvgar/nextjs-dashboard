9e849c40fd7c24538188b1d9f64744df401f9b4b
Daniel Garcia <124107401+danvgar@users.noreply.github.com>
2024-01-21 17:25:46 -0800

    Updated customers page, based on example from github.com/vercel/next-learn repo.

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,10 +1,26 @@
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
     title: 'Customers',
 };
 
+export default async function Page({
+    searchParams,
+}: {
+    searchParams?: {
+        query?: string;
+        page?: string;
+    };
+}) {
+    const query = searchParams?.query || '';
 
-export default function Page() {
-    return <p> Customers Page</p>
+    const customers = await fetchFilteredCustomers(query);
+
+    return (
+        <main>
+            <CustomersTable customers={customers} />
+        </main>
+    );
 }
